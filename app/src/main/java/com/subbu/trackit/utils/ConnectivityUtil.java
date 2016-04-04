db0f5947cae043fa358390b163e920b38101eb4f
subrahmanyam yepuri <subbdev@gmail.com>
2016-04-04 21:02:55 +0530

    1.Refactor (IN progress)

---
+++ app/src/main/java/com/subbu/trackit/utils/ConnectivityUtil.java
@@ -0,0 +1,68 @@
+package com.subbu.trackit.utils;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.location.LocationManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+import com.subbu.trackit.R;
+
+/**
+ * Created by Subrahmanyam.yepuri on 04-04-2016.
+ */
+public class ConnectivityUtil {
+    public static void check(final Activity activity) {
+        if (isConnectingToInternet(activity)) {
+            checkGPSStatus(activity);
+        } else {
+            final AlertDialog alertDialog = new AlertDialog.Builder(activity).create();
+
+            // Setting Dialog Title
+            alertDialog.setTitle("Track me");
+
+            // Setting Dialog Message
+            alertDialog.setMessage(activity.getString(R.string.network_turn_on));
+            alertDialog.setCanceledOnTouchOutside(false);
+
+            // Setting OK Button
+            alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    alertDialog.dismiss();
+                    checkGPSStatus(activity);
+                }
+            });
+            // Showing Alert Message
+            alertDialog.show();
+        }
+    }
+
+    public static void checkGPSStatus(Activity activity) {
+        LocationManager locationManager = (LocationManager) activity.getSystemService(Context.LOCATION_SERVICE);
+        // getting GPS status
+        boolean isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+        // getting network status
+        boolean isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+        if (!(isGPSEnabled && isNetworkEnabled)) {
+            Util.showSettingsAlert(activity);
+        }
+    }
+
+    public static boolean isConnectingToInternet(Context context) {
+        ConnectivityManager connectivity = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivity != null) {
+            NetworkInfo[] info = connectivity.getAllNetworkInfo();
+            if (info != null)
+                for (int i = 0; i < info.length; i++)
+                    if (info[i].getState() == NetworkInfo.State.CONNECTED) {
+                        return true;
+                    }
+
+        }
+        return false;
+    }
+}
